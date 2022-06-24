2ea36ee15fdc17dd11a6027f304798e119b97d77
JoszyAD <108139183+JoszyAD@users.noreply.github.com>
2022-06-24 15:46:19 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function subcribe(){
+    document.querySelector(".s-form").style.display="block";
+    document.querySelector("#sub-btn").style.display="none";
+}
+function submitted(){
+    document.querySelector(".s-form").style.display="none";
+    document.querySelector(".result").style.display="block";
+}
